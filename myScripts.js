e75af2b192e216d66f8c6da130b63934746626fd
Wintertime10 <159956992+Wintertime10@users.noreply.github.com>
2024-11-13 10:36:16 -0600

    Add files via upload

---
+++ myScripts.js
@@ -0,0 +1,51 @@
+function startTime() {
+   const today = new Date();
+   let h = today.getHours();
+   let m = today.getMinutes();
+   let s = today.getSeconds();
+   m = checkTime(m);
+   s = checkTime(s);
+   document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+   setTimeout(startTime, 1000);
+}
+  
+function checkTime(i) {
+   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+   return i;
+}
+  
+
+
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+ }
+  
+ function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+ }
+ var show = false;
+function showNote(){
+   if (show == false){
+       document.getElementById("note").style.opacity = 1;
+       show = true;
+   }
+   else {
+       document.getElementById("note").style.opacity = 0;
+       show = false;
+   }
+}
+
+
+var showCalc = false;
+function showCalculator(){
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
+}
